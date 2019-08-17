200588f8b03e307bf6d72047d6694c79bc7b125a
Daniel Jhin Yoo <danieljyoo@gmail.com>
2019-08-17 11:29:22 -0700

    update the ctx.env.LOG_LEVEL if log level is dynamically updated

--- test/level.test.js
+++ test/level.test.js
@@ -12,6 +12,7 @@ describe('LogLevelPlugin', () => {
       },
       context: { },
       state: { },
+      env: { LOG_LEVEL: 'info' },
       logger: new LoggerWrapper()
     }
     plugin = new LogLevelPlugin()
@@ -20,46 +21,57 @@ describe('LogLevelPlugin', () => {
     ctx.event.headers['X-Log-Level'] = 'debug'
     await plugin.request(ctx, async () => {
       expect(ctx.logger.level()).toEqual('debug')
+      expect(ctx.env.LOG_LEVEL).toEqual('debug')
     })
     expect(ctx.logger.level()).toEqual('info')
+    expect(ctx.env.LOG_LEVEL).toEqual('info')
   })
   it ('should also support x-correlation-log-level', async () => {
     ctx.event.headers['x-correlation-log-level'] = 'debug'
     await plugin.request(ctx, async () => {
       expect(ctx.logger.level()).toEqual('debug')
+      expect(ctx.env.LOG_LEVEL).toEqual('debug')
     })
     expect(ctx.logger.level()).toEqual('info')
+    expect(ctx.env.LOG_LEVEL).toEqual('info')
   })
   it ('should noop if set to same level', async () => {
     ctx.event.headers['X-Log-Level'] = 'info'
     await plugin.request(ctx, async () => {
       expect(ctx.logger.level()).toEqual('info')
+      expect(ctx.env.LOG_LEVEL).toEqual('info')
     })
     expect(ctx.logger.level()).toEqual('info')
+    expect(ctx.env.LOG_LEVEL).toEqual('info')
   })
   it ('should set level if level is uppercase', async () => {
     ctx.event.headers['X-Log-Level'] = 'DEBUG'
     await plugin.request(ctx, async () => {
       expect(ctx.logger.level()).toEqual('debug')
+      expect(ctx.env.LOG_LEVEL).toEqual('debug')
     })
     expect(ctx.logger.level()).toEqual('info')
+    expect(ctx.env.LOG_LEVEL).toEqual('info')
   })
   it ('should noop if no headers set', async () => {
     ctx.event.headers['X-Log-Level'] = 'info'
     await plugin.request(ctx, async () => {
       expect(ctx.logger.level()).toEqual('info')
+      expect(ctx.env.LOG_LEVEL).toEqual('info')
     })
     expect(plugin.original).toBe(null)
+    expect(ctx.env.LOG_LEVEL).toEqual('info')
   })
   it ('should not set level if level is null', async () => {
     ctx.event.headers['X-Log-Level'] = ''
     await plugin.request(ctx, noop)
     expect(plugin.original).toBe(null)
+    expect(ctx.env.LOG_LEVEL).toEqual('info')
   })
   it ('should not set level if level is invalid', async () => {
     ctx.event.headers['X-Log-Level'] = 'invalid'
     await plugin.request(ctx, noop)
     expect(plugin.original).toBe(null)
+    expect(ctx.env.LOG_LEVEL).toEqual('info')
   })
-
 })
